a0716206bdf8d282ee64f56d2e71e96a2680fe6b
Sherin Mary Rajee <118478759+sherin-11@users.noreply.github.com>
2024-04-11 13:28:17 +0530

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Sherin Mary Rajee</h1>
+        <p className={styles.description}>
+          I'm a full-stack developer with an experience in using React and
+          NodeJS!
+        </p>
+        <button  className={styles.contactBtn}>
+          Contact Me
+        </button>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
